3aec4f1ad2664321c54741ec66da34d06f1b746b
Georgios Margarone <g.margarone@bueroparallel.de>
2024-11-25 09:06:56 +0100

    Update path-examples and fix typo in delete_ds_store script.

--- delete_ds_store.sh
+++ delete_ds_store.sh
@@ -4,10 +4,10 @@
 
 # To automate the script creat a cron job e.g.:
 # 1. use the command: `crontab -e`
-# 2. * * * * * ~/PhpstormProjects/delete_ds_Store.sh
+# 2. * * * * * ~/path/example/delete_ds_store.sh
 # 3. Save the file. That`s it.
 
-# List of directories in which .DS_Store files are to be deleteid.
+# List of directories in which .DS_Store files are to be deleted.
 # Remove the example paths and edit them to suit your needs.
 DIRECTORIES=(
     "/path/example"
@@ -16,7 +16,7 @@ DIRECTORIES=(
 
 # Optional:
 # to activate logging, remove the # and then create a .logfile and adjust the path accordingly.
-# LOG_FILE="$HOME/PhpstormProjects/delete_ds_Store.logfile"
+# LOG_FILE="$HOME/path/example/delete_ds_store.logfile"
 
 log_message() {
     local MESSAGE="$1"
